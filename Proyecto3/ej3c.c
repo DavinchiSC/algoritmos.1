546ade3505ea8ecb263f5bddfa7e626f12ab7c9d
DavinchiSC <david.cortes@mi.unc.edu.ar>
2023-05-14 23:39:18 -0300

    correccion

--- Proyecto3/ej3c.c
+++ Proyecto3/ej3c.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <assert.h>
 int main (void) {
     int x,y;
     printf ("Introduzca el valor de x\n");
     scanf ("%d", &x);
     printf ("Introduzca el valor de y\n");
     scanf ("%d", &y);
+    assert ((x == 4 && y == 5) || (x == 3 && y == 6) || (2 == 1 && y == 1));
     y = y + y;
     printf ("y = %d", y);
     x = x + y;
