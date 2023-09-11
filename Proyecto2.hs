dbbb5ae547e4f8ac5dc5a324632215e5f756394a
DavinchiSC <david.cortes@mi.unc.edu.ar>
2023-09-11 18:29:24 -0300

    Update Proyecto2.hs

--- Proyecto2.hs
+++ Proyecto2.hs
@@ -40,3 +40,49 @@ minimoElemento (x:xs) = min x (minimoElemento xs)
 minimoElemento' :: (Bounded a, Ord a) => [a] -> a
 minimoElemento' [] = maxBound
 minimoElemento' (x:xs) = min x (minimoElemento' xs)
+
+--c
+
+--ghci> minimoElemento [Fa, La, Sol, Re, Fa]
+--Re
+
+--Ejercicio 4
+--a
+
+type Altura = Int
+type NumCamiseta = Int
+data Zona = Arco | Defensa | Mediocampo | Delantera
+data TipoReves = DosManos | UnaMano
+data Modalidad = Carretera | Pista | Monte | BMX
+data PiernaHabil = Izquierda | Derecha
+type ManoHabil = PiernaHabil
+data Deportista = Ajedrecista | Ciclista Modalidad | Velocista Altura | Tenista TipoReves ManoHabil Altura | Futbolista Zona NumCamiseta PiernaHabil Altura
+
+--b: Ciclista :: Modaliad -> Deportista
+
+--c
+
+contar_velocistas :: [Deportista] -> Int
+contar_velocistas [] = 0
+contar_velocistas ((Velocista _):xs) = 1 + contar_velocistas xs
+contar_velocistas (_:xs) = contar_velocistas xs
+
+--d
+
+mismaZona :: Zona -> Deportista -> Bool
+mismaZona Arco (Futbolista Arco _ _ _) = True
+mismaZona Defensa (Futbolista Defensa _ _ _) = True
+mismaZona Mediocampo (Futbolista Mediocampo _ _ _) = True
+mismaZona Delantera (Futbolista Delantera _ _ _) = True
+mismaZona _ _ = False
+
+
+contar_futbolistas :: [Deportista] -> Zona -> Int
+contar_futbolistas [] z = 0
+contar_futbolistas (x:xs) z | mismaZona z x = 1 + contar_futbolistas xs z
+                            | otherwise = contar_futbolistas xs z
+
+--e
+
+contar_futbolistas' :: [Deportista] -> Zona -> Int
+contar_futbolistas' xs z = length (filter (mismaZona z) xs)
